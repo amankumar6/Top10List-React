b4e7f29b404783c29f31af4d88764bec6d53a8ca
amankumar6 <amankumar786dpsdh@gmail.com>
2021-01-26 18:28:36 +0530

    chore: faq content

---
+++ src/components/Content/faq.js
@@ -0,0 +1,14 @@
+const faq = [
+  {
+    header: "What is Top10List? What kind of a site/resource is it?",
+    content:
+      "Top10List is a Website, which is made by Aman Kumar. It is a website where you can see the Top 10 List of Anime, Games, Movies, Music Videos, and Web Series.",
+  },
+  {
+    header: "What is your Aim?",
+    content:
+      "My aim is to make everyday life more comfortable with this website. I found that people generally have trouble finding which anime, Web Series, Movies is right, and which should they watch first. So they start searching their Top 10 List, and after reading 3-4 articles, they get confused because of the inconsistency of the information. And nowadays there are many fake sites which misguide you and give false information. <br /> So, I decided to create a trustworthy site that you can blindly rely on.",
+  },
+];
+
+export default faq;
